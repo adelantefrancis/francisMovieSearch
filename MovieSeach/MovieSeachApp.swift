b01ecf52204090435924b53958ed3722826fcf5e
francisstar <francis@starpay.com.ph>
2022-06-17 21:43:14 +0800

    Initial Commit

---
+++ MovieSeach/MovieSeachApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSeachApp.swift
+//  MovieSeach
+//
+//  Created by Francis Adelante on 6/17/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieSeachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
